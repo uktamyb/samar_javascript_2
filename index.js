6692182976e7681fc1019ef17f2261d0ec8be1f8
unknown <uktamyb@gmail.com>
2021-07-15 10:24:48 +0500

    Uchalasi ham ulandi

---
+++ index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// const userAge = confirm("Are you 18?"); // brauzerga alert chiqaradi.
+// console.log(userAge);
+
+// const userName = prompt("ismingiz kim?", "Ismingizni kiriting:")
+// console.log(userName);
+
+// const answers = [];
+// answers[0] = prompt("Ismingiz?");
+// answers[1] = prompt("Familiyangiz?");
+// answers[2] = prompt("Yoshingiz?");
+
+// console.log(answers);
+
+// const firstName = "Uktam";
+// const age = 38;
+// // ma'lumotlarni quyidagidek qilib yozish "interpolatsiya" deyiladi
+// console.log(`Mening's ismim ${firstName} va yoshim ${age}.`);
+
+let x = 10;
+console.log(x--);
+console.log(x);
